54e6b3dc7d22b8c13378bcf8672e0d1818f869ae
Vijay Singh <vr97288@gmail.com>
2024-01-23 21:20:45 +0530

    ligh and dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+ // Function to set the theme and update UI
+ function setTheme(theme) {
+    document.body.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    var switchThemeBtn = document.getElementById('switchTheme');
+    if (switchThemeBtn) {
+        switchThemeBtn.innerHTML = theme === 'dark' ?  '<i class="bi bi-sun-fill"></i>' : '<i class="bi bi-moon-stars-fill"></i>';
+    }
+    console.log(`Switched to ${theme} theme`);
+}
 
+var currentTheme = localStorage.getItem('theme') || 'dark';
+setTheme(currentTheme);
+
+// Event listener for the switch theme button
+var switchThemeBtn = document.getElementById('switchTheme');
+if (switchThemeBtn) {
+    switchThemeBtn.addEventListener('click', () => {
+        currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        setTheme(currentTheme);
+    });
+}
